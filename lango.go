c3e89f4c28536074a98bea3f3a675cc7ba4c6392
Keith Avery <slabgorb@gmail.com>
2017-03-14 09:06:03 -0400

    code cleanup

--- lango.go
+++ lango.go
@@ -4,41 +4,39 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"math/rand"
-	"time"
 	"io/ioutil"
+	"math/rand"
 	"regexp"
+	"strings"
+	"time"
 )
-
-var source = rand.NewSource(time.Now().UnixNano())
-var random = rand.New(source)
 
 type CharacterList struct {
 	characters map[rune]int
 }
 
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+const (
+	START = '^'
+	END   = '$'
+)
+
 type Chain struct {
-	lookback int
-	start rune
-	end rune
+	lookback       int
 	characterLists map[string]*CharacterList
 }
 
-func NewCharacterList() (*CharacterList) {
-	var characterList CharacterList
-	characterList.characters = make(map[rune]int)
-	return &characterList
+func NewCharacterList() *CharacterList {
+	return &CharacterList{characters: make(map[rune]int)}
 }
 
-func NewChain(lookback int) (*Chain) {
-	chain := Chain{lookback: lookback, start: '^', end: '$'}
-	chain.characterLists = make(map[string]*CharacterList)
-	return &chain
+func NewChain(lookback int) *Chain {
+	return &Chain{lookback: lookback, characterLists: make(map[string]*CharacterList)}
 }
 
 func (chain *Chain) AddRune(key string, char rune) {
-	_,ok := chain.characterLists[key]
+	_, ok := chain.characterLists[key]
 	if !ok {
 		chain.characterLists[key] = NewCharacterList()
 	}
@@ -51,41 +49,42 @@ func (chain *Chain) AddWord(word string) {
 		chain.AddRune(key, char)
 		keyRunes, key = rotateKey(keyRunes, char)
 	}
-	chain.AddRune(key, chain.end)
-	
+	chain.AddRune(key, END)
+
 }
 
 func (chain *Chain) NewKey() ([]rune, string) {
 	keyRunes := make([]rune, chain.lookback)
-	for index,_ := range keyRunes {
-		keyRunes[index] = chain.start
+	for index, _ := range keyRunes {
+		keyRunes[index] = START
 	}
 	return keyRunes, string(keyRunes)
-	
+
 }
 
 func rotateKey(key []rune, char rune) ([]rune, string) {
-	key = append(key, char)
-	key = key[1:]
+	key = append(key, char)[1:]
 	return key, string(key)
 }
 
 func (chain *Chain) MakeWord() (word string) {
-	keyRunes, key :=  chain.NewKey()
+	keyRunes, key := chain.NewKey()
 	for {
 		char := chain.characterLists[key].Choose(len(word))
-		if char == chain.end { break }
+		if char == END {
+			break
+		}
 		word += string(char)
 		keyRunes, key = rotateKey(keyRunes, char)
 
 	}
-	return 
+	return
 }
 
 func (characterList *CharacterList) TotalCounts() (total int) {
 	total = 0
 	for _, value := range characterList.characters {
-    total = total + value
+		total = total + value
 	}
 	return
 }
@@ -98,39 +97,47 @@ func (characterList *CharacterList) Choose(wordLength int) (choice rune) {
 		if randomNumber <= position {
 			choice = char
 		}
-		
+
 	}
 	return
 }
 
-
-func readCorpus(path string)  []string  {
+func readCorpus(path string) []string {
 	re := regexp.MustCompile(`[^\p{L}]`)
 	dat, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
-	words := strings.Split(string(dat), " ")
+	rawWords := strings.Split(string(dat), " ")
+	var words []string
+	for _, word := range rawWords {
+		if word != "" {
+			words = append(words, word)
+		}
+	}
 	for index, word := range words {
 		words[index] = re.ReplaceAllString(word, "")
 	}
 	return words
 }
 
-
 func main() {
 	wordSlices := make(chan []string)
 	var words []string
 	chain := NewChain(2)
-	go func() { wordSlices <- readCorpus("/Users/keithavery/Projects/fantasy-language-maker/corpus/french.txt") }()
-	go func() { wordSlices <- readCorpus("/Users/keithavery/Projects/fantasy-language-maker/corpus/english.txt") }()
+	go func() {
+		wordSlices <- readCorpus("/Users/keithavery/Projects/fantasy-language-maker/corpus/french.txt")
+	}()
+	go func() {
+		wordSlices <- readCorpus("/Users/keithavery/Projects/fantasy-language-maker/corpus/english.txt")
+	}()
 	for i := 0; i < 2; i++ {
-		result := <- wordSlices
+		result := <-wordSlices
 		words = append(words, result...)
 	}
-	for _,word := range words {
+	for _, word := range words {
 		chain.AddWord(word)
 	}
 	fmt.Println(chain.MakeWord())
-	
+
 }
